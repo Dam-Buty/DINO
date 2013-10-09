adc5fc51b083083685fdebc3b90687a441bc107e
Chad <dam.buty@gmail.com>
2013-10-08 19:33:13 -0500

    Jauges OK

--- www/js/queue.js
+++ www/js/queue.js
@@ -36,44 +36,15 @@ var handle_drop = function(evt) {
 var refresh_liste = function() {
     queue.sort(compare);
     
-    $("#files-list").empty();
+    $("#files-list li").detach();
     
     $.each(queue, function(i, document) {
-        var custom_class;
-        
-        switch(document.status) {
-            case -1:
-                custom_class = "idle";
-                custom_text = "En fila";
-                break;
-            
-            case 0:
-                custom_class = "uploading";
-                custom_text = "";
-                break;
-                
-            case 1:
-                custom_class = "ready";
-                custom_text = "Cargado";
-                break;
-        }
-        
-        
-        var document_li = $("<li></li>");
-        
-        document_li.addClass(custom_class)
-        .append(this.document.name + " - <i>" + custom_text + "</i>");
-        
-        $("#files-list").append(document_li);
-        
-        queue[i].li = document_li;
+        $("#files-list").append(queue[i].li);
     });
 };
 
 var upload = function(file, uploader, queue_position) {
     var upload_data = new FormData;
-    
-    refresh_liste();
     
     upload_data.append("document", file);
 
@@ -89,8 +60,11 @@ var upload = function(file, uploader, queue_position) {
             if(myXhr.upload){ // if upload property exists
                 myXhr.upload.addEventListener('progress', function(evt) {
                     if(evt.lengthComputable){
+                        var pourcentage = Math.floor((evt.loaded * 100) / evt.total);
+                        
                         document_li = queue[queue_position].li;
-                        document_li.find("i").text(evt.loaded + " / " + evt.total);
+                        document_li.css("background-size", pourcentage + "%" + " 100%");
+                        document_li.find("span").text(pourcentage + "%");
                     }
                 }, false);
             }
@@ -99,13 +73,25 @@ var upload = function(file, uploader, queue_position) {
         success: function() {
             uploading[uploader] = undefined;
             queue[queue_position].status = 1;
-            document_li = queue[queue_position].li;
-            document_li.find("i").text("OK");
+            
+            queue[queue_position].li
+            .removeClass()
+            .css("background-size", "0 0")
+            .addClass("done")
+            .find("span").text("OK");
+            
             handle_uploads();
         },
         error: function() {
-            document_li = queue[queue_position].li;
-            document_li.find("i").text("KO");
+            uploading[uploader] = undefined;
+            queue[queue_position].status = -2;
+            
+            queue[queue_position].li
+            .removeClass()
+            .css("background-size", "0 0")
+            .addClass("error")
+            .find("span").text("KO");
+            
             handle_uploads();
         }
     });
@@ -116,12 +102,15 @@ var upload = function(file, uploader, queue_position) {
 ///////////////////:
 // Cherche un uploader libre et lui attribue le premier fichier de la queue
 var handle_uploads = function() {
+    refresh_liste();
+    
     $.each(uploading, function(i, uploader) {
         if (uploader === undefined) {
             $.each(queue, function(j, document) {
                 if (document.status == -1) {
                     queue[j].status = 0;
                     uploading[i] = document.document;
+                    queue[j].li.removeClass().addClass("uploading");
                     upload(document.document, i, j);
                     return false;
                 }
@@ -130,7 +119,7 @@ var handle_uploads = function() {
     });
 }
 
-///////////////////////////:
+////////////////////////////
 // Gestion des fichiers entrés dans l'input
 var handle_files = function() {
     files = $("#files-handler").prop("files");
@@ -141,59 +130,15 @@ var handle_files = function() {
         
         // Si l'extension est légale, on pousse le fichier dans la queue
         if (extension in allowed_extensions) {
-            queue.push({ document: this, status: -1, size: this.size, li: undefined });
+            var document_li = $("<li></li>");
+            
+            document_li.removeClass().addClass("idle")
+            .append(this.name + " - <span>En fila</span>");
+            
+            queue.push({ document: this, status: -1, size: this.size, li: document_li });
         }
     });
     
     handle_uploads();
 };
-
-var assign_worker = function() {
-    //console.log("Checking for free workers");
-    if (queue.length > 0) {
-        var free_workers = [];
-        var done = 0;
-        
-        // On fait un tableau des workers libres
-        for (var i = 0; i < workers.length; i++) {
-            if (workers[i] === undefined) {
-                free_workers.push(i);
-            }
-        }
-        
-        // Si on a au moins un worker libre on parcourt la queue
-        if (free_workers.length > 0) {
-            $.each(queue, function() {
-                // Si on trouve un document éligible on l'assigne au premier worker libre, qu'on supprime du tableau
-                if (this.status == 0) {
-                    workers[free_workers[0]] = this;
-                    console.log("Affected worker " + free_workers[0] + " to file " + this.name);
-                    this.status = 1;
-                    
-                    // lancement du WebWorker
-                    var worker = new Worker('js/worker.js');
-                    
-                    worker.addEventListener('message', function(e) {
-                      console.log('Worker said: ', e.data);
-                    }, false);
-                    
-                    worker.postMessage(workers[free_workers[0]]);
-                    
-                    free_workers.splice(0, 1);
-                }
-                
-                if (free_workers.length == 0) {
-                    return false;
-                }
-            });
-        } else {
-            //console.log("None Found");
-        }
-    } else {
-        //console.log("Queue empty");
-    }
-}
-
-// On vérifie toutes les secondes si il y a un worker qui glande
-// window.setInterval(assign_worker, 5000);
 
