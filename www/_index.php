33fbd2cbd3ac2e1eb304ca88b3f688f53fafc12a
Damien BUTY <dam.buty@gmail.com>
2014-01-20 14:24:52 -0600

    Intégration pure chat

--- www/_index.php
+++ www/_index.php
@@ -211,6 +211,9 @@ if ($_SESSION["niveau"] >= 30) {
 <script type="text/javascript">
     var profil = undefined;
 </script>
+
+<script type='text/javascript'>(function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://www.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({ c: '15d4cfc0-8fae-4e70-8294-389f583f897f', f: true }); done = true; } }; })();</script>
+
 </body>
 </html>
 
