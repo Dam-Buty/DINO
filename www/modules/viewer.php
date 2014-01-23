e82a8bbe7708857609616578aa9d56d7f7cde230
Damien BUTY <dam.buty@gmail.com>
2014-01-22 19:27:35 -0600

    versionage de pdfjs

--- www/modules/viewer.php
+++ www/modules/viewer.php
@@ -9,7 +9,7 @@ if (isset($_SESSION["user"])) {
     if ($extension == "pdf") {
 #        echo $display;
 #        echo urlencode("../../do/doUnpack.php?document=" . $filename . "&display=" . $display);
-        header("Location: ../pdfjs/web/viewer.html?file=" . urlencode("../../do/doUnpack.php?document=" . $filename . "&display=" . $display));
+        header("Location: ../pdfjs/web/viewer.html?file=" . urlencode("../../do/doUnpack.php?document=" . $filename . "&display=" . $display . "&v=1"));
     } else {
     ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
