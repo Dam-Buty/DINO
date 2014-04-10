ca54e1dea3d27f3c2017e875eefae7a08ba9ec5a
Damien BUTY <dam.buty@gmail.com>
2014-04-10 15:06:13 -0500

    Details are comments

--- www/modules/tuto/tuto-1.php
+++ www/modules/tuto/tuto-1.php
@@ -1,5 +1,5 @@
 <div class="etape" id="etape-1-0">
-    <h1><b>1</b>/<b>8</b> - Creacion de mundo</h1>
+    <h1><b>1</b>/<b>6</b> - Creacion de mundo</h1>
     <p>Para adaptar exactamente tu perfil <b>DINO</b> a tus necesidades, puedes crear tu proprio mundo desde zero.</p>
     <p>Puede parecer un poco tecnico, pero no te asustes, estamos aqui para acompanarte en cada etapa.</p>
     <p>Antes de empezar, te aconsejo leer nuestros <a href="http://prezi.com/7rblvsge3xfs/" target="_blank">8 tips para un perfil perfecto</a>.</p>
@@ -8,31 +8,31 @@
     <div class="boutons next">Andamos!</div>
 </div>
 <div class="etape" id="etape-1-1">
-    <h1><b>2</b>/<b>8</b> - Creacion de mundo</h1>
+    <h1><b>2</b>/<b>6</b> - Creacion de mundo</h1>
     <p>Puedes crear un nuevo mundo en cualquier momento dando click en el globo (<img src="img/world_black_20.png" style="vertical-align: middle;">) en la parte de izquierda.</p>
 </div>
 <div class="etape" id="etape-1-2">
-    <h1><b>3</b>/<b>8</b> - Primer campo</h1>
+    <h1><b>3</b>/<b>6</b> - Primer campo</h1>
     <p>En DINO, los documentos se clasifican por <b>campos</b>. Por ejemplo, las <b>Facturas</b> se clasifican por <b>Cliente</b> y las <b>Nominas</b> por <b>Empleado</b>.</p>
     <p>Para crear tu mundo, necesitas crear a lo menos un <b>campo</b>, dando click en el boton <img src="img/add_champ_20.png" style="vertical-align: middle;"/>.</p>
 </div>
 <div class="etape" id="etape-1-3">
-    <h1><b>3</b>/<b>8</b> - Primer campo</h1>
+    <h1><b>3</b>/<b>6</b> - Primer campo</h1>
     <p>Inscribe el <b>Nombre</b> de tu campo y da click en "Guardar".</p>
 </div>
 <div class="etape" id="etape-1-4">
-    <h1><b>4</b>/<b>8</b> - Primer campo</h1>
+    <h1><b>4</b>/<b>6</b> - Primer campo</h1>
     <p>En cada momento, puedes ver la estructura de tu <b>perfil documental</b> en la parte de izquierda.</p>
     <p>Da click en el boton <img src="img/add_20.png" style="vertical-align: middle;"/> para agregar un primer <b>tipo de documento</b> al campo <b class="tuto-champ"></b>.</p>
 </div>
 <div class="etape" id="etape-1-5">
-    <h1><b>5</b>/<b>8</b> - Primer documento</h1>
+    <h1><b>5</b>/<b>6</b> - Primer documento</h1>
     <p>Ingresa el nombre de un <b>tipo de documento</b> que quieres asociar a tus <b class="tuto-champ"></b>(s).</p>
     <p>El <a href="http://www.dino.mx/university/los-diferentes-niveles-de-usuarios-dino/" target="_blank"><b>nivel de seguridad</b></a> determina quiene puede consultar los <b>documentos</b>. Si tienes una duda, puedes dejar el nivel "<b>Visitante</b>".</p>
-    <p>Un documento tambien puede ser <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>mensual</b></a> o tomar un <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>detalle</b></a>.</p>
+    <p>Un documento tambien puede ser <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>mensual</b></a> o acceptar un <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>comentario</b></a>.</p>
 </div>
 <div class="etape" id="etape-1-6">
-    <h1><b>6</b>/<b>8</b> - Nivel Experto!</h1>
+    <h1><b>6</b>/<b>6</b> - Nivel Experto!</h1>
     <p>Perfecto! Ahora que sabes todo de los <b>campos</b> y <b>tipos de documentos</b>, te dejo experimentar con la creacion de tu <b>perfil</b>.</p>
     <p>Puedes probar las funcionalidades, y cuando tu perfil te satisfece picar en el boton "<b>Publicar</b>". Podras modificar tu perfil en cualquier momento dando click en el boton <img src="img/profil_hover_30.png" style="vertical-align: middle; width: 20px;"/>.</p>
     <p><i>Si tienes una duda, te invito a consultar nuestros <a href="http://prezi.com/7rblvsge3xfs/" target="_blank">8 tips para un perfil perfecto</a> y nuestros <a href="http://prezi.com/i1z9x8l-77l7/" target="_blank">3 ejemplos de mundos documentales</a>.</i></p>
