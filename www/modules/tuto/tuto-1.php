d11e41a1a2f58b04600a17624041f11d3530051e
Damien BUTY <dam.buty@gmail.com>
2014-04-11 17:32:50 -0500

    Fixes #12

--- www/modules/tuto/tuto-1.php
+++ www/modules/tuto/tuto-1.php
@@ -1,11 +1,11 @@
 <div class="dialog-box etape" id="etape-1-0">
     <h1>Creacion de mundo</h1>
     <div class="popup-ligne"><div class="popup-jauge jauge-14"></div></div>
-    <p>Vamos a crear un <b>mundo documental</b> en blanco, para que puedas adaptarle exactamente a tus necesidades.</p>
+    <p>Vamos a crear un <b>mundo documental</b> en blanco, para que puedas adaptarlo exactamente a tus necesidades.</p>
     <p>Antes de empezar, te aconsejo leer nuestros <a href="http://prezi.com/7rblvsge3xfs/" target="_blank">8 tips para un perfil perfecto</a>.</p>
     <iframe src="http://prezi.com/embed/7rblvsge3xfs/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;features=undefined&amp;disabled_features=undefined" width="100%" height="300" frameBorder="0" webkitAllowFullScreen mozAllowFullscreen allowfullscreen></iframe>
     <p style="text-align: center;">(<i>En fullscreen se ve mucho mejor!</i>)</p>
-    <div class="boutons next">Andamos!</div>
+    <div class="boutons next">Vamos</div>
 </div>
 <div class="dialog-box etape" id="etape-1-1">
     <h1>Creacion de mundo</h1>
@@ -33,15 +33,15 @@
 <div class="dialog-box etape" id="etape-1-5">
     <h1>Primer documento</h1>
     <div class="popup-ligne"><div class="popup-jauge jauge-84"></div></div>
-    <p>Ingresa el nombre de un <b>tipo de documento</b> asociado a tus <b class="tuto-champ"></b>(s).</p>
-    <p>El <a href="http://www.dino.mx/university/los-diferentes-niveles-de-usuarios-dino/" target="_blank"><b>nivel de seguridad</b></a> determina quiene puede consultar los <b>documentos</b>. Si tienes una duda, puedes dejar el nivel "<b>Visitante</b>".</p>
+    <p>Ingresa el nombre de un <b>tipo de documento</b> asociado a tu <b class="tuto-champ"></b>.</p>
+    <p>El <a href="http://www.dino.mx/university/los-diferentes-niveles-de-usuarios-dino/" target="_blank"><b>nivel de seguridad</b></a> determina quiene puede consultar los <b>documentos</b>.</p>
     <p>Un <b>documento</b> tambien puede ser <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>mensual</b></a> o acceptar un <a href="http://www.dino.mx/university/los-diferentes-atributos-disponibles-para-un-documento/" target="_blank"><b>comentario</b></a>.</p>
 </div>
 <div class="dialog-box etape" id="etape-1-6">
     <h1>Nivel Experto!</h1>
     <div class="popup-ligne"><div class="popup-jauge jauge-100"></div></div>
     <p>Perfecto! Ahora que sabes todo de los <b>campos</b> y <b>tipos de documentos</b>, te dejo experimentar con la creacion de tu <b>perfil</b>.</p>
-    <p>Puedes probar las funcionalidades, y cuando tu perfil te satisfece picar "<b>Guardar</b>". Podras modificar tu perfil en cualquier momento dando click en el boton <img src="img/profil_hover_30.png" style="vertical-align: middle; width: 20px;"/>.</p>
+    <p>Puedes probar las funcionalidades, y cuando tu perfil te satisface picar "<b>Guardar</b>". Podras modificar tu perfil en cualquier momento dando click en el boton <img src="img/profil_hover_30.png" style="vertical-align: middle; width: 20px;"/>.</p>
     <p><i>Si tienes una duda, te invito a consultar nuestros <a href="http://prezi.com/7rblvsge3xfs/" target="_blank">8 tips para un perfil perfecto</a> y nuestros <a href="http://prezi.com/i1z9x8l-77l7/" target="_blank">3 ejemplos de mundos documentales</a>.</i></p>
-    <div class="boutons next">Hasta luego!</div>
+    <div class="boutons next">Hasta pronto</div>
 </div>
